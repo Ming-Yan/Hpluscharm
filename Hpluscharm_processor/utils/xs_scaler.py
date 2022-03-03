6caadba6d4054525c325e17c12f688797c4b4987
Ming-Yan Lee <a342545@gmail.com>
2022-03-03 15:09:16 +0100

    fix:

--- Hpluscharm_processor/utils/xs_scaler.py
+++ Hpluscharm_processor/utils/xs_scaler.py
@@ -1,17 +1,22 @@
-def read_xs(file):
+import os
+def read_xs(files):
     import json
 
-    f = open("../metadata/%s" %file)
+    f = open(files)
     data = json.load(f)
     xs_dict={}
     for obj in data:
         xs_dict[obj['process_name']]=float(obj['cross_section'])
     return xs_dict
-def scale_xs(hist,lumi,events,xsfile="../metadata/xsection.json"):
-    xs_dict = read_xs(xsfile)
+def scale_xs(hist,lumi,events,unscale=False,xsfile="metadata/xsection.json"):
+
+    xs_dict = read_xs(os.getcwd()+"/"+xsfile)
     scales={}
+
     for key in events:
         if type(key) != str or key=="Data": continue
-        scales[key]=xs_dict[key]*lumi/events[key]
+        if unscale: 
+            scales[key]=events[key]/xs_dict[key]*lumi
+        else :scales[key]=xs_dict[key]*lumi/events[key]
     hist.scale(scales, axis="dataset")
     return hist
